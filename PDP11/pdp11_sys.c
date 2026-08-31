ffddc5027c79b65bfa7a002ce9cfc0391b205a16
Kyle Duren <pixitha.kyle@gmail.com>
2026-08-30 22:12:25 -0400

    PDP11: Register DMV device

--- PDP11/pdp11_sys.c
+++ PDP11/pdp11_sys.c
@@ -118,6 +118,7 @@ extern DEVICE xu_dev, xub_dev;
 extern DEVICE ke_dev;
 extern DEVICE kg_dev;
 extern DEVICE dmc_dev;
+extern DEVICE dmv_dev;
 extern DEVICE dup_dev;
 extern DEVICE dpv_dev;
 extern DEVICE kmc_dev;
@@ -206,6 +207,7 @@ DEVICE *sim_devices[] = {
     &xub_dev,
     &kg_dev,
     &dmc_dev,
+    &dmv_dev,
     &dup_dev,
     &dpv_dev,
     &kmc_dev,
